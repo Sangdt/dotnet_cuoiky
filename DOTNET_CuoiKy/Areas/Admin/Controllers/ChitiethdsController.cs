bb5ede3018f09a62b787da9ff6dec711e4588d4f
= <NHAN.VT2126@DESKTOP-UL5REKE>
2019-06-23 23:39:41 +0700

    i had finished abt event log in and out. With Admin can add, fix and del many kinds of record like products, bills, etc. However, template in here is too terible, so i can improve that in next days.

---
+++ DOTNET_CuoiKy/Areas/Admin/Controllers/ChitiethdsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DOTNET_CuoiKy.Models.DB;
+
+namespace DOTNET_CuoiKy.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class ChitiethdsController : Controller
+    {
+        private readonly comdatabaseContext _context;
+
+        public ChitiethdsController(comdatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: admin/Chitiethds
+        public async Task<IActionResult> Index()
+        {
+            var comdatabaseContext = _context.Chitiethd.Include(c => c.IdHdNavigation).Include(c => c.IdSpNavigation);
+            return View(await comdatabaseContext.ToListAsync());
+        }
+
+        // GET: admin/Chitiethds/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chitiethd = await _context.Chitiethd
+                .Include(c => c.IdHdNavigation)
+                .Include(c => c.IdSpNavigation)
+                .FirstOrDefaultAsync(m => m.IdHd == id);
+            if (chitiethd == null)
+            {
+                return NotFound();
+            }
+
+            return View(chitiethd);
+        }
+
+        // GET: admin/Chitiethds/Create
+        public IActionResult Create()
+        {
+            ViewData["IdHd"] = new SelectList(_context.Hoadon, "Idhoadon", "Idhoadon");
+            ViewData["IdSp"] = new SelectList(_context.Sanpham, "IdsanPham", "IdsanPham");
+            return View();
+        }
+
+        // POST: admin/Chitiethds/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdHd,IdSp,Soluong,GiaSp")] Chitiethd chitiethd)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(chitiethd);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdHd"] = new SelectList(_context.Hoadon, "Idhoadon", "Idhoadon", chitiethd.IdHd);
+            ViewData["IdSp"] = new SelectList(_context.Sanpham, "IdsanPham", "IdsanPham", chitiethd.IdSp);
+            return View(chitiethd);
+        }
+
+        // GET: admin/Chitiethds/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chitiethd = await _context.Chitiethd.FindAsync(id);
+            if (chitiethd == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdHd"] = new SelectList(_context.Hoadon, "Idhoadon", "Idhoadon", chitiethd.IdHd);
+            ViewData["IdSp"] = new SelectList(_context.Sanpham, "IdsanPham", "IdsanPham", chitiethd.IdSp);
+            return View(chitiethd);
+        }
+
+        // POST: admin/Chitiethds/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdHd,IdSp,Soluong,GiaSp")] Chitiethd chitiethd)
+        {
+            if (id != chitiethd.IdHd)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(chitiethd);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ChitiethdExists(chitiethd.IdHd))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdHd"] = new SelectList(_context.Hoadon, "Idhoadon", "Idhoadon", chitiethd.IdHd);
+            ViewData["IdSp"] = new SelectList(_context.Sanpham, "IdsanPham", "IdsanPham", chitiethd.IdSp);
+            return View(chitiethd);
+        }
+
+        // GET: admin/Chitiethds/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chitiethd = await _context.Chitiethd
+                .Include(c => c.IdHdNavigation)
+                .Include(c => c.IdSpNavigation)
+                .FirstOrDefaultAsync(m => m.IdHd == id);
+            if (chitiethd == null)
+            {
+                return NotFound();
+            }
+
+            return View(chitiethd);
+        }
+
+        // POST: admin/Chitiethds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var chitiethd = await _context.Chitiethd.FindAsync(id);
+            _context.Chitiethd.Remove(chitiethd);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ChitiethdExists(int id)
+        {
+            return _context.Chitiethd.Any(e => e.IdHd == id);
+        }
+    }
+}
---
+++ DOTNET_CuoiKy/Areas/Admin/Controllers/HoadonsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DOTNET_CuoiKy.Models.DB;
+
+namespace DOTNET_CuoiKy.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class HoadonsController : Controller
+    {
+        private readonly comdatabaseContext _context;
+
+        public HoadonsController(comdatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: admin/Hoadons
+        public async Task<IActionResult> Index()
+        {
+            var comdatabaseContext = _context.Hoadon.Include(h => h.IdNguoimuaNavigation);
+            return View(await comdatabaseContext.ToListAsync());
+        }
+
+        // GET: admin/Hoadons/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hoadon = await _context.Hoadon
+                .Include(h => h.IdNguoimuaNavigation)
+                .FirstOrDefaultAsync(m => m.Idhoadon == id);
+            if (hoadon == null)
+            {
+                return NotFound();
+            }
+
+            return View(hoadon);
+        }
+
+        // GET: admin/Hoadons/Create
+        public IActionResult Create()
+        {
+            ViewData["IdNguoimua"] = new SelectList(_context.Khachhang, "IdKhachHang", "Email");
+            return View();
+        }
+
+        // POST: admin/Hoadons/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Idhoadon,IdNguoimua,IdchiTiet,TongTien,SoLuong,NgayTao,TinhTrang")] Hoadon hoadon)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hoadon);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdNguoimua"] = new SelectList(_context.Khachhang, "IdKhachHang", "Email", hoadon.IdNguoimua);
+            return View(hoadon);
+        }
+
+        // GET: admin/Hoadons/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hoadon = await _context.Hoadon.FindAsync(id);
+            if (hoadon == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdNguoimua"] = new SelectList(_context.Khachhang, "IdKhachHang", "Email", hoadon.IdNguoimua);
+            return View(hoadon);
+        }
+
+        // POST: admin/Hoadons/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Idhoadon,IdNguoimua,IdchiTiet,TongTien,SoLuong,NgayTao,TinhTrang")] Hoadon hoadon)
+        {
+            if (id != hoadon.Idhoadon)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hoadon);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HoadonExists(hoadon.Idhoadon))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdNguoimua"] = new SelectList(_context.Khachhang, "IdKhachHang", "Email", hoadon.IdNguoimua);
+            return View(hoadon);
+        }
+
+        // GET: admin/Hoadons/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hoadon = await _context.Hoadon
+                .Include(h => h.IdNguoimuaNavigation)
+                .FirstOrDefaultAsync(m => m.Idhoadon == id);
+            if (hoadon == null)
+            {
+                return NotFound();
+            }
+
+            return View(hoadon);
+        }
+
+        // POST: admin/Hoadons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var hoadon = await _context.Hoadon.FindAsync(id);
+            _context.Hoadon.Remove(hoadon);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HoadonExists(int id)
+        {
+            return _context.Hoadon.Any(e => e.Idhoadon == id);
+        }
+    }
+}
